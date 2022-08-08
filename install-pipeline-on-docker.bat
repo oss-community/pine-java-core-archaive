ad1e71966bede1c84f12192e76e02ca990f3e17a
Saman Alishirishahrbabak <samanalishiri@gmail.com>
2022-08-08 23:04:29 +0200

    temp commit

--- install-pipeline-on-docker.bat
+++ install-pipeline-on-docker.bat
@@ -1,5 +1,5 @@
+call cls
 call .\pipeline\docker\pre-config.bat
-call .\pipeline\docker\setenv.bat
 call .\pipeline\docker\createenv.bat
 
 docker-compose -f .\pipeline\docker\docker-compose.yaml ^
--- pipeline/docker/createenv.bat
+++ pipeline/docker/createenv.bat
@@ -1,14 +1,13 @@
+echo GITHUB_JENKINS_TOKEN=%GITHUB_JENKINS_TOKEN% > .\pipeline\docker\.env
 
-echo GITHUB_JENKINS_TOKEN=%GITHUB_JENKINS_TOKEN% > .\.env
+echo SONAR_DB=%SONAR_DB% >> .\pipeline\docker\.env
+echo SONAR_DB_USER=%SONAR_DB_USER% >> .\pipeline\docker\.env
+echo SONAR_DB_PASSWORD=%SONAR_DB_PASSWORD% >> .\pipeline\docker\.env
+echo SONAR_JDBC_URL=%SONAR_JDBC_URL% >> .\pipeline\docker\.env
 
-echo SONAR_DB=%SONAR_DB% >> .\.env
-echo SONAR_DB_USER=%SONAR_DB_USER% >> .\.env
-echo SONAR_DB_PASSWORD=%SONAR_DB_PASSWORD% >> .\.env
-echo SONAR_JDBC_URL=%SONAR_JDBC_URL% >> .\.env
-
-echo JFROG_DB=%JFROG_DB% >> .\.env
-echo JFROG_DB_USER=%JFROG_DB_USER% >> .\.env
-echo JFROG_DB_PASSWORD=%JFROG_DB_PASSWORD% >> .\.env
-echo JF_SHARED_DATABASE_TYPE=%JF_SHARED_DATABASE_TYPE% >> .\.env
-echo JF_SHARED_DATABASE_DRIVER=%JF_SHARED_DATABASE_DRIVER% >> .\.env
-echo JF_SHARED_DATABASE_URL=%JF_SHARED_DATABASE_URL% >> .\.env
+echo JFROG_DB=%JFROG_DB% >> .\pipeline\docker\.env
+echo JFROG_DB_USER=%JFROG_DB_USER% >> .\pipeline\docker\.env
+echo JFROG_DB_PASSWORD=%JFROG_DB_PASSWORD% >> .\pipeline\docker\.env
+echo JF_SHARED_DATABASE_TYPE=%JF_SHARED_DATABASE_TYPE% >> .\pipeline\docker\.env
+echo JF_SHARED_DATABASE_DRIVER=%JF_SHARED_DATABASE_DRIVER% >> .\pipeline\docker\.env
+echo JF_SHARED_DATABASE_URL=%JF_SHARED_DATABASE_URL% >> .\pipeline\docker\.env
