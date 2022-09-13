89eecae58e19886a3c367af61f5784140da1c88c
Saman Alishirishahrbabak <samanalishiri@gmail.com>
2022-09-13 22:55:54 +0200

    temp commit

--- concourse-credentials-generator.bat
+++ concourse-credentials-generator.bat
@@ -1,6 +1,6 @@
-REM echo # credentials > .\ci\concourse\credentials.yml
-REM echo github-user-name: %GITHUB_USERNAME% >> .\ci\concourse\credentials.yml
-REM echo github-user-email: %GITHUB_EMAIL% >> .\ci\concourse\credentials.yml
+echo # credentials > .\ci\concourse\credentials.yml
+echo github-user-name: %GITHUB_USERNAME% >> .\ci\concourse\credentials.yml
+echo github-user-email: %GITHUB_EMAIL% >> .\ci\concourse\credentials.yml
 
 echo github-package-token: %GITHUB_PACKAGE_TOKEN% >> .\ci\concourse\credentials.yml
 echo github-artifactory-urL: %GITHUB_ARTIFACTORY_URL% >> .\ci\concourse\credentials.yml
@@ -8,10 +8,10 @@ echo jfrog-artifactory-username: %JFROG_ARTIFACTORY_USERNAME% >> .\ci\concourse\
 echo jfrog-artifactory-password: %JFROG_ARTIFACTORY_PASSWORD% >> .\ci\concourse\credentials.yml
 echo sonar-token: %SONAR_TOKEN% >> .\ci\concourse\credentials.yml
 
-REM echo github-key-pub: >> .\ci\concourse\credentials.yml
-REM echo github-key: ^|- >> .\ci\concourse\credentials.yml
-REM mkdir .\ssh
-REM call ssh-keygen -t rsa -C "concourse_team" -f .\ssh\id_rsa
+echo github-key-pub: >> .\ci\concourse\credentials.yml
+echo github-key: ^|- >> .\ci\concourse\credentials.yml
+mkdir .\ssh
+call ssh-keygen -t rsa -C "concourse_team" -f .\ssh\id_rsa
 REM del .\ssh\id_rsa
 REM del .\ssh\id_rsa.pub
 REM rmdir .\ssh
