ad8fbb104d64ba92aa8081cdc4f214eac51aa4dd
Saman Alishirishahrbabak <samanalishiri@gmail.com>
2022-10-31 18:43:24 +0100

    temp commit

--- pipeline-install.bat
+++ pipeline-install.bat
@@ -1,10 +1,12 @@
-# call cls
-# call .\docker-pre-config.bat
-# call .\docker-create-env.bat
+call cls
+
+call .\docker-pre-config.bat
+call .\docker-create-env.bat
+call .\concourse-generate-credentials.bat
+
 # call .\docker-build-image-openjdk17.bat
 # call .\docker-build-image-jenkins.bat
 # call .\docker-build-image-nexus.bat
 # call .\docker-build-image-pinealpine.bat
-# call .\concourse-generate-credentials.bat
 
 docker compose --file .\docker\docker-compose.yml --project-name pine-pipeline --env-file .\docker\.env up --build -d
